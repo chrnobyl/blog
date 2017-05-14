d7d469ab047b60c38d5693c17f8772fecf6e9176
github username <github email address>
2017-05-14 17:22:30 -0400

    built out sessions controller, users controller, users index, posts index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,25 @@
 class SessionsController < ApplicationController
-
+  before_action :logged_in?, except: [:new, :create]
+  
   def new
 
   end
 
   def create
-    byebug
-    @session = User.find_by(params[:user][:username])
+    # byebug
+    @user = User.find_by(username: params[:user][:username])
+    if !@user.nil? && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Invalid credentials, please try again"
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   resources :post_tags, only: [:create, :destroy]
   resources :tags, only: [:create, :destroy]
   resources :comments
-  resources :posts
+  resources :posts, except: :index
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: 'sessions#new', as: 'new_session'
-  post '/', to: 'sessions#create', as: 'login'
+  get '/login', to: 'sessions#new', as: 'new_session'
+  post '/login', to: 'sessions#create', as: 'login'
+
+  get '/', to: 'posts#index'
+
 end
